471270ef649066ce39ae1b93bdcf1e93e7211ae5
Danica Gazdić <danicagazdic@gmail.com>
2022-08-25 19:36:12 +0200

    Changed data type for interval and timestamp to int64

--- TokenBucket/tokenBucket.go
+++ TokenBucket/tokenBucket.go
@@ -5,36 +5,36 @@ import (
 )
 
 const (
-	DEFAULT_RESET_INTERVAL = "10s"
+	DEFAULT_RESET_INTERVAL = 10
 	DEFAULT_MAX_TOKEN_NUM = 10
 )
 
 type TokenBucket struct{
-	lastReset time.Time
+	lastReset int64
 	maxTokenNum int
 	tokenNum int
-	resetInterval time.Duration
+	resetInterval int64
 }
 
-func tokenBucketConstructor(maxTokenNum int, resetInterval string) *TokenBucket{
+func tokenBucketConstructor(maxTokenNum int, resetInterval int64) *TokenBucket{
 	tb := TokenBucket{}
-	tb.lastReset = time.Now()
+	tb.lastReset = time.Now().Unix()
 	if maxTokenNum <= 0{
 		maxTokenNum = DEFAULT_MAX_TOKEN_NUM
 	}
 	tb.maxTokenNum = maxTokenNum
 	tb.tokenNum = 0
-	if resetInterval == ""{
+	if resetInterval <= 0{
 		resetInterval = DEFAULT_RESET_INTERVAL
 	}
-	tb.resetInterval, _ = time.ParseDuration(resetInterval)
+	tb.resetInterval = resetInterval
 
 	return &tb
 }
 
 func (tokenBucket *TokenBucket) processRequest() bool{
-	currentTime := time.Now()
-	if currentTime.Sub(tokenBucket.lastReset) >= tokenBucket.resetInterval{
+	currentTime := time.Now().Unix()
+	if  currentTime - tokenBucket.lastReset >= tokenBucket.resetInterval{
 		tokenBucket.lastReset = currentTime
 		tokenBucket.tokenNum = 0
 
