61f6893f42fa2d5c8821c69735b5ff64dc4f6249
REX1389 <rrvlelcc@gmail.com>
2022-08-27 15:22:54 +0200

    Bloom Filter
    CreateBloom(int capacity, float64 ratio) враћа показивач на интерфејс који има две функције:
    void Add(string s)
    bool MaybeContains(string s)
    које додају нови стринг и проверавају да ли је прослеђени стринг можда у Блум структури респективно.
    
    Параметар capacity представља очекивани број података које Блум треба да запамти.
    Параметар ratio представља прецизност филтера тј. однос између исправних и неисправних потврдних одговора. Колико схватам, што је број већи, то је Блум већи, дакле прецизнији.

---
+++ bloom.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/spaolacci/murmur3"
+	"hash"
+	"math"
+	"time"
+)
+
+type BloomFilter interface {
+	Add(s string)
+	MaybeContains(s string) bool
+}
+
+type BloomStruct struct {
+	data     []byte
+	hash     []hash.Hash32
+	dataSize int
+	hashSize int
+}
+
+func (bs BloomStruct) Add(s string) {
+	for i := 0; i < bs.hashSize; i++ {
+		bs.hash[i].Write([]byte(s))
+		index := bs.hash[i].Sum32() % uint32(bs.dataSize)
+		bs.hash[i].Reset()
+		bs.data[index] = 1
+	}
+}
+
+func (bs BloomStruct) MaybeContains(s string) bool {
+	for i := 0; i < bs.hashSize; i++ {
+		bs.hash[i].Write([]byte(s))
+		index := bs.hash[i].Sum32() % uint32(bs.dataSize)
+		bs.hash[i].Reset()
+		if bs.data[index] != 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func CreateHashFunctions(hashSize int) []hash.Hash32 {
+	hash := []hash.Hash32{}
+	ts := uint(time.Now().Unix())
+	for i := 1; i <= hashSize; i++ {
+		hash = append(hash, murmur3.New32WithSeed(uint32(ts+1)))
+	}
+	return hash
+}
+
+func CreateBloom(capacity int, ratio float64) *BloomFilter {
+	dataSize := int(math.Ceil(float64(capacity) * math.Abs(math.Log(ratio)) / math.Pow(math.Log(2), float64(2))))
+	hashSize := int(math.Ceil((float64(dataSize) / float64(capacity)) * math.Log(2)))
+	var bf BloomFilter = BloomStruct{
+		dataSize: dataSize,
+		hashSize: hashSize,
+		data:     make([]byte, dataSize),
+		hash:     CreateHashFunctions(hashSize),
+	}
+	return &bf
+}
