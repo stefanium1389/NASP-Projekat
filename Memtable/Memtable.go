07c4dd091edf850bd4a7b724e24034599e9d15cd
Danica Gazdić <danicagazdic@gmail.com>
2022-08-30 14:32:12 +0200

    bzvz

---
+++ Memtable/Memtable.go
@@ -0,0 +1,126 @@
+package Memtable
+
+import (
+	"fmt"
+	"main/SkipList"
+)
+
+//var memTableNum = 0
+
+const (
+	maxLevel = 10
+)
+
+type Memtable struct {
+	Skiplist    *SkipList.Skiplist // podaci
+	threshold   int                // maksimalni kapacitet tj. prag zapisa (kad se dosegne, vrsi se flus\
+	currentSize int                // trenutna velicina svih elemenata ukupno
+
+}
+
+func NewMemtable(threshold int, maxLevel int, probability float32) *Memtable {
+	return &Memtable{
+		Skiplist:    SkipList.NewSkipList(maxLevel, probability),
+		threshold:   threshold,
+		currentSize: 0,
+	}
+}
+
+func (mt *Memtable) CurrentSize() int {
+	return mt.currentSize
+}
+
+func (mt *Memtable) toFlush() bool {
+	elements := mt.currentSize + 1
+	if mt.threshold <= elements{
+		return true
+	} else {
+		return false
+	}
+}
+
+func (mt *Memtable) Insert(key string, value []byte) bool {
+	node := mt.Find(key) // ovo je kljucna funkcija
+	if node != nil {
+		if node.Tombstone == true {
+			node.Tombstone = false
+			return true
+		}
+	} else {
+		toFlush := mt.toFlush()
+		if toFlush == false {
+			mt.currentSize++
+			return mt.Skiplist.Insert(key, value)
+		}else{
+			return false
+		}
+
+	}
+
+	return false
+}
+
+//func (mt *Memtable) Flush(){
+//	//SSTable.Flush(mt)
+//
+//	mt.Empty()
+//}
+
+func (mt *Memtable) Find(key string) *SkipList.Skipnode {
+	node, _ := mt.Skiplist.Search(key)
+	if node != nil {
+		return node
+	}
+	return nil
+}
+
+func (mt *Memtable) FindAndDelete(key string) bool {
+	node := mt.Find(key)
+	if node != nil {
+		if node.Tombstone == false{
+			node.Tombstone = true
+		}
+		return true
+	}
+	return false
+}
+
+func (mt *Memtable) Empty() {
+	mt.currentSize = 0
+	mt.Skiplist.Empty()
+}
+
+func (mt *Memtable) PrintMt() {
+	fmt.Println("Threshold", mt.threshold)
+	fmt.Println("Current size of Memtable: ", mt.currentSize)
+	mt.Skiplist.DisplayAll()
+}
+
+func (mt *Memtable) GetSL() *SkipList.Skiplist {
+	return mt.Skiplist
+}
+
+func (mt *Memtable) GetThreshold() int {
+	return mt.threshold
+}
+
+
+func (mt *Memtable) Test() {
+	//mt.Insert("1", []byte("pozdrav1"))
+	mt.Insert("2", []byte("pozdrav2"))
+	mt.Insert("4", []byte("pozdrav4"))
+	mt.Insert("6", []byte("pozdrav6"))
+	mt.Insert("5", []byte("pozdrav5"))
+	mt.Insert("3", []byte("pozdrav3"))
+	mt.Insert("1", []byte("pozdrav1"))
+	mt.Insert("10", []byte("111"))
+	mt.Insert("7", []byte("23r"))
+
+	//node := mt.Find("2")
+	//fmt.Printf(string(node.Value) + "\n")
+	//
+	//mt.FindAndDelete("6")
+	mt.PrintMt()
+	//node = mt.Find("5")
+	//fmt.Printf(string(node.Value))
+}
---
+++ main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"main/CountMinSketch"
+	"main/HyperLogLog"
+	"main/Processor"
+	"main/SSTable"
+	"os"
+	"strings"
+)
+
+var processor *Processor.Processor
+
+func ReplaceWhiteSpace(str string) string {
+	str = strings.Replace(str, "\n", "", 1)
+	str = strings.Replace(str, "\r", "", 1)
+	return str
+}
+
+func ReadInput(put bool) (string, string) {
+	fmt.Print("Unesite kljuc: ")
+	reader := bufio.NewReader(os.Stdin)
+	key, _ := reader.ReadString('\n')
+	key = ReplaceWhiteSpace(key)
+	if !put {
+		return key, ""
+	}
+	fmt.Print("Unesite vrednost: ")
+	value, _ := reader.ReadString('\n')
+	value = ReplaceWhiteSpace(value)
+	return key, value
+}
+
+func Put() {
+	key, value := ReadInput(true)
+	success := processor.Put(key, []byte(value))
+
+	if success {
+		fmt.Println("Dodavanje je uspesno.")
+	} else {
+		fmt.Println("Dodavanje nije uspelo.")
+	}
+}
+
+func Delete() {
+	key, _ := ReadInput(false)
+	deleted := processor.Delete(key)
+	if !deleted {
+		fmt.Println("Neuspesno brisanje. ")
+	} else {
+		fmt.Println("Uspesno obrisano. ")
+	}
+}
+
+func HLL() {
+	fmt.Println("1. Izaberi postojeci HLL")
+	fmt.Println("2. Dodaj HLL")
+	fmt.Println("x. Odustani")
+	reader := bufio.NewReader(os.Stdin)
+	choice, err := reader.ReadString('\n')
+	choice = ReplaceWhiteSpace(choice)
+	if err != nil {
+		panic(err.Error())
+	}
+	if choice == "1" {
+		key, _ := ReadInput(false)
+		key += "_hll"
+		data, found := processor.Get(key)
+		if !found {
+			fmt.Println("Ne postoji HLL sa datim kljucem. ")
+			return
+		}
+		hll := HyperLogLog.HyperLogLog{}
+		hll.Decode(data.Value)
+	} else if choice == "2" {
+		key, _ := ReadInput(false)
+		key += "_hll"
+		_, found := processor.Get(key)
+		if found {
+			fmt.Println("Vec postoji HLL sa ovim kljucem. ")
+			return
+		}
+		hll := HyperLogLog.NewHyperLogLog(6)
+		data := hll.Encode()
+		processor.Put(key, data)
+		fmt.Println("Uspesno je dodat novi HLL")
+
+	} else if choice != "x" {
+		fmt.Println("Nepostojeca opcija. ")
+	}
+}
+
+func CMS() {
+	fmt.Println("1. Izaberi postojeci CMS")
+	fmt.Println("2. Dodaj novi CMS")
+	fmt.Println("x. Odustani")
+	reader := bufio.NewReader(os.Stdin)
+	choice, err := reader.ReadString('\n')
+	choice = ReplaceWhiteSpace(choice)
+	if err != nil {
+		panic(err.Error())
+	}
+	if choice == "1" {
+		key, _ := ReadInput(false)
+		key += "_cms"
+		data, found := processor.Get(key)
+		if !found {
+			fmt.Println("Ne postoji CMS sa datim kljucem. ")
+			return
+		}
+		cms := CountMinSketch.CountMinSketch{}
+		cms.Decode(data.Value)
+	} else if choice == "2" {
+		key, _ := ReadInput(false)
+		key += "_cms"
+		_, found := processor.Get(key)
+		if found {
+			fmt.Println("Vec postoji CMS sa ovim kljucem. ")
+			return
+		}
+		cms := CountMinSketch.NewCountMinSketch(0.1, 0.1)
+		data := cms.Encode()
+		processor.Put(key, data)
+		fmt.Println("Uspesno je dodat novi CMS")
+
+	} else if choice != "x" {
+		fmt.Println("Nepostojeca opcija. ")
+	}
+
+}
+
+func Get() {
+	key, _ := ReadInput(false)
+	val, flag := processor.Get(key)
+	if flag {
+		SSTable.PrintElement(&val)
+	}
+}
+
+func main() {
+	processor = Processor.NewProcessor()
+	//SSTable.Flush(&Memtable.Memtable{})
+	Menu()
+
+}
+
+func Menu() {
+	for true {
+		fmt.Println("\n\nIzaberite zeljenu operaciju: ")
+		fmt.Println("1. Put")
+		fmt.Println("2. Get")
+		fmt.Println("3. Delete")
+		fmt.Println("4. Compaction")
+		fmt.Println("5. HLL")
+		fmt.Println("6. CMS")
+		fmt.Println("x. Izlaz")
+		fmt.Print("Vas izbor: ")
+
+		reader := bufio.NewReader(os.Stdin)
+		choice, err := reader.ReadString('\n')
+		choice = ReplaceWhiteSpace(choice)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		switch choice {
+		case "1":
+			Put()
+			break
+		case "2":
+			Get()
+			break
+		case "3":
+			Delete()
+			break
+		case "4":
+			break
+		case "5":
+			HLL()
+			break
+		case "6":
+			CMS()
+			break
+		case "x":
+			return
+		default:
+			fmt.Println("Pogresan unos.")
+		}
+	}
+}
